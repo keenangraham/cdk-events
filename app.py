fb670056d33bed3509f108ed9f7d4914c14215ba
Keenan Graham <ksgraham@stanford.edu>
2022-07-22 13:35:42 -0700

    Add event stack

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 from aws_cdk import App
+from aws_cdk import Stack
+
+from aws_cdk.aws_chatbot import SlackChannelConfiguration
 
 from shared_infrastructure.cherry_lab.environments import US_WEST_2
 
 
 app = App()
 
+
+class EventNotficationStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs: Any) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        event_channel = SlackChannelConfiguration(
+            self,
+            'EventChannel',
+            slack_channel_configuration_name='aws-events',
+            slack_workspace_id='T1KMV4JJZ',
+            slack_channel_id='C03QPGPLAMQ',
+        )
+
+EventNotficationStack(
+    app,
+    'EventNotficationStack',
+    env=US_WEST_2,
+)
 
 app.synth()
